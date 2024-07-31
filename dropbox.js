651f13eb1da44a965bf7e5ffad25b6e43140da8d
RAMKUMAR V <126764479+ramkumardev07@users.noreply.github.com>
2024-07-31 12:10:26 +0530

    chore: no change in code

--- dropbox.js
+++ dropbox.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const path = require('path')
 const { response } = require('./routes/Register.route')
 const dbx = new dropbox({
-    accessToken:"l.B5FA6Ro2lGuXJAr_Ssf3HYod7wJrJUZ5cN42mWJwJ2G9U5fkzy9QU9dVVmYzjRmfB86hcWWC4_4pj7CkS8XWNKfmZwnBRwNUWJotpBON43Lvt0iRAblTL_zyJ3_D3wFXnD8ixddjYh5Mm2w"
+    accessToken:"drop box access token"
     })
 const imageurl = async (dropboxFilepath) =>{
     const response = await dbx.filesGetTemporaryLink({path: dropboxFilepath})
